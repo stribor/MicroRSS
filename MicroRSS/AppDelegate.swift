c4fe7e62dfb0059752689355e8c1c1694ce4daab
IvanG <ivang@mail.com>
2026-06-26 12:38:09 +0200

    Add edit menu shortcuts

--- MicroRSS/AppDelegate.swift
+++ MicroRSS/AppDelegate.swift
@@ -4,8 +4,44 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusController: StatusMenuController?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.mainMenu = ApplicationMenu.make()
         let store = FeedStore()
         let service = RSSService()
         statusController = StatusMenuController(store: store, service: service)
+    }
+}
+
+enum ApplicationMenu {
+    static func make() -> NSMenu {
+        let mainMenu = NSMenu()
+        mainMenu.addItem(appMenuItem())
+        mainMenu.addItem(editMenuItem())
+        return mainMenu
+    }
+
+    private static func appMenuItem() -> NSMenuItem {
+        let item = NSMenuItem()
+        let menu = NSMenu(title: "MicroRSS")
+        menu.addItem(NSMenuItem(title: "Quit MicroRSS", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        item.submenu = menu
+        return item
+    }
+
+    private static func editMenuItem() -> NSMenuItem {
+        let item = NSMenuItem()
+        let menu = NSMenu(title: "Edit")
+
+        menu.addItem(NSMenuItem(title: "Undo", action: Selector(("undo:")), keyEquivalent: "z"))
+        menu.addItem(NSMenuItem(title: "Redo", action: Selector(("redo:")), keyEquivalent: "Z"))
+        menu.addItem(.separator())
+        menu.addItem(NSMenuItem(title: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x"))
+        menu.addItem(NSMenuItem(title: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c"))
+        menu.addItem(NSMenuItem(title: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v"))
+        menu.addItem(NSMenuItem(title: "Delete", action: #selector(NSText.delete(_:)), keyEquivalent: ""))
+        menu.addItem(.separator())
+        menu.addItem(NSMenuItem(title: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a"))
+
+        item.submenu = menu
+        return item
     }
 }
